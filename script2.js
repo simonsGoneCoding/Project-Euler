966b5d2df3676a1a46a0649e8c799c394bf7b9bb
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-06-24 21:13:58 +0100

    Even Fibonacci numbers

--- script2.js
+++ script2.js
@@ -1,16 +1,19 @@
 const fibonacci = function () {
-  let numbers = [1, 2];   // two starting fibonacci values 
+  let numbers = [1, 2]; // two starting fibonacci values 
   const startingLenght = numbers.length
-  for (i = 0; i < 20 - startingLenght; i++) {
-    let j = i + 1;
-    let nextNumber = numbers[i] + numbers[j]
-    numbers.push(nextNumber);
-    // console.log(nextNumber)
+  let nextNumber = 0;
+  for (i = 0; i < 1000 - startingLenght; i++) {
+    if (nextNumber < 4000000) {
+      let j = i + 1;
+      nextNumber = numbers[i] + numbers[j]
+      numbers.push(nextNumber);
+    }
   }
-  return function () {   //closure return function
+  numbers.pop() //last index in array has value grater than 4000000
+  return function () { //closure return function
     let sum = 0;
     for (i = 0; i < numbers.length; i++) {
-      if (numbers[i] % 2 === 0) {  //only even numbers
+      if (numbers[i] % 2 === 0) { //only even numbers
         sum += numbers[i];
       }
     }
